820a944d43648edcf91c3759a2d113468e79fea4
Andrew Hudak <andrewhudak@Fictivs-MacBook-Pro-92.local>
2018-05-19 17:34:25 -0700

    for warrick, my love

---
+++ strandreverse_pot.ino
@@ -0,0 +1,78 @@
+// Simple strand test for Adafruit Dot Star RGB LED strip.
+// This is a basic diagnostic tool, NOT a graphics demo...helps confirm
+// correct wiring and tests each pixel's ability to display red, green
+// and blue and to forward data down the line.  By limiting the number
+// and color of LEDs, it's reasonably safe to power a couple meters off
+// the Arduino's 5V pin.  DON'T try that with other code!
+
+#include <Adafruit_DotStar.h>
+// Because conditional #includes don't work w/Arduino sketches...
+#include <SPI.h>         // COMMENT OUT THIS LINE FOR GEMMA OR TRINKET
+//#include <avr/power.h> // ENABLE THIS LINE FOR GEMMA OR TRINKET
+
+#define NUMPIXELS 72 // Number of LEDs in strip
+
+// Here's how to control the LEDs from any two pins:
+#define DATAPIN    4
+#define CLOCKPIN   5
+Adafruit_DotStar strip = Adafruit_DotStar(
+  NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
+// The last parameter is optional -- this is the color data order of the
+// DotStar strip, which has changed over time in different production runs.
+// Your code just uses R,G,B colors, the library then reassigns as needed.
+// Default is DOTSTAR_BRG, so change this if you have an earlier strip.
+
+// Hardware SPI is a little faster, but must be wired to specific pins
+// (Arduino Uno = pin 11 for data, 13 for clock, other boards are different).
+//Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DOTSTAR_BRG);
+
+void setup() {
+
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
+  clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
+#endif
+
+  strip.begin(); // Initialize pins for output
+  strip.show();  // Turn all LEDs off ASAP
+  Serial.begin(9600);
+  
+  
+}
+
+// Runs 10 LEDs at a time along strip, cycling through red, green and blue.
+// This requires about 200 mA for all the 'on' pixels + 1 mA per 'off' pixel.
+
+int      head  = 0, tail = -10; // Index of first 'on' and 'off' pixels
+int      dir  = 1;            // Direction of snake
+int      potpin = 0;
+int      velo;
+uint32_t color = 0xFF0000;      // 'On' color (starts red)
+
+void loop() {
+  velo = analogRead(potpin);
+  velo = map(velo, 0, 1023, 0, 500);
+  strip.setBrightness(20);
+  strip.setPixelColor(head, color); // 'On' pixel at head
+  strip.setPixelColor(tail, 0);     // 'Off' pixel at tail
+  strip.show();                     // Refresh strip
+  delay(velo);                        // Pause 20 milliseconds (~50 FPS)
+  if (dir == 1) {
+    head++;
+  } else {
+    head--;
+  }
+  if(head == NUMPIXELS || head == 0) {         // Increment head index.  Off end of strip?
+    tail = head + dir*11;
+    dir = -dir;                       //  Yes, reset head index to start
+    if((color >>= 8) == 0)          //  Next color (R->G->B) ... past blue now?
+      color = 0xFF0000;             //   Yes, reset to red
+  }
+  if (dir ==1) {
+    tail++;
+  } else {
+    tail--;
+  }
+  Serial.print(tail);
+  Serial.print(' ');
+  //if(tail >= NUMPIXELS) tail = 0; // Increment, reset tail index
+}
